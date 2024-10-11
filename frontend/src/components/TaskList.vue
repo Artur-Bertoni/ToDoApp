8c502f68a2e4e4f6b2475743c70d2888c578a7f3
Artur Bertoni <artur_b_santos@hotmail.com>
2024-10-11 13:19:51 -0300

    Remake no front

--- frontend/src/components/TaskList.vue
+++ frontend/src/components/TaskList.vue
@@ -1,34 +1,56 @@
 <template>
   <div id="app">
     <div class="content-wrapper">
-      <h1>TO DO LIST</h1>
+      <div class="header">
+        <h1>TO DO LIST</h1>
+        <button @click="openPopup" class="icon-button add-task-button">
+          <img src="../assets/img/add-icon.png" alt="Add Task" class="icon-image add-task-image"/>
+        </button>
+      </div>
+
       <ul class="task-list-container">
         <li v-for="task in tasks" :key="task.id" :class="{ 'completed-task': task.completed }">
           <div class="task-container">
-            <div :class="['task-name', { completed: task.completed }]">{{ task.name }}</div>
-            <div :class="['task-description', { completed: task.completed }]">{{ task.description }}</div>
-            <div class="task-deadline">Deadline: {{ new Date(task.deadline).toLocaleDateString('pt-BR') }}</div>
+            <input type="checkbox" :checked="task.completed" @change="toggleCompletion(task.id)"
+                   class="complete-checkbox"/>
+            <div class="task-details">
+              <div :class="['task-name', { completed: task.completed }]">{{ task.name }}</div>
+              <div :class="['task-description', { completed: task.completed }]">{{ task.description }}</div>
+              <div
+                  :class="['task-deadline', { 'past-deadline': new Date(task.deadline) < new Date() }, { completed: task.completed }]">
+                Deadline: {{ new Date(task.deadline).toLocaleDateString('pt-BR') }}
+              </div>
+            </div>
           </div>
           <div class="button-group">
-            <button @click="toggleCompletion(task.id)">{{ task.completed ? 'Uncomplete' : 'Complete' }}</button>
-            <button @click="editTask(task)">Edit</button>
-            <button @click="deleteTask(task.id)" class="delete-button">Delete</button>
+            <button @click="editTask(task)" class="icon-button">
+              <img src="../assets/img/edit-icon.png" alt="Edit" class="icon-image"/>
+            </button>
+            <button @click="deleteTask(task.id)" class="icon-button delete-button">
+              <img src="../assets/img/delete-icon.png" alt="Delete" class="icon-image"/>
+            </button>
           </div>
         </li>
       </ul>
-      <form @submit.prevent="addTask">
-<!--        TODO adicionar validação para data inválida (ex:11/11/111111111)-->
-<!--        TODO adicionar responsividade para campos de add task-->
-<!--        TODO substituir botões de add, complete, edit e delete por ícones-->
-<!--        TODO edit e add devem abrir um pop-up-->
-        <input v-model="newTask.name" placeholder="Task name" required />
-        <input v-model="newTask.description" placeholder="Task description" required />
-        <input type="date" v-model="newTask.deadline" placeholder="Deadline" />
-        <button type="submit">{{ editingTask ? 'Edit Task' : 'Add Task' }}</button>
-        <button v-if="editingTask" type="button" @click="cancelEdit">Cancel</button>
-      </form>
+
+      <div v-if="showPopup" class="popup-overlay" @click.self="closePopup">
+        <div class="popup">
+          <h2>{{ editingTask ? 'Edit Task' : 'Add Task' }}</h2>
+          <form @submit.prevent="addTask">
+            <input v-model="newTask.name" placeholder="Task name" required/>
+            <input v-model="newTask.description" placeholder="Task description" required/>
+            <input type="date" v-model="newTask.deadline" @input="validateDateLength" />
+            <div class="popup-buttons">
+              <button type="submit">{{ editingTask ? 'Save Task' : 'Add Task' }}</button>
+              <button type="button" @click="closePopup">Cancel</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
+
+
 </template>
 
 <script>
@@ -37,7 +59,8 @@ export default {
     return {
       tasks: [],
       newTask: {name: "", description: "", deadline: "", completed: false},
-      editingTask: null
+      editingTask: null,
+      showPopup: false
     };
   },
   methods: {
@@ -59,7 +82,7 @@ export default {
             .then(data => {
               console.log("Task added successfully");
               this.fetchTasks();
-              this.resetForm();
+              this.closePopup();
             })
             .catch(error => {
               console.error('There was a problem with the fetch operation:', error);
@@ -85,7 +108,7 @@ export default {
             });
       }
 
-      this.resetForm();
+      this.closePopup();
     },
     editTask(task) {
       this.newTask = {
@@ -96,8 +119,14 @@ export default {
         completed: task.completed
       };
       this.editingTask = task;
+      this.showPopup = true;
     },
-    cancelEdit() {
+    openPopup() {
+      this.resetForm();
+      this.showPopup = true;
+    },
+    closePopup() {
+      this.showPopup = false;
       this.resetForm();
     },
     resetForm() {
@@ -125,12 +154,21 @@ export default {
             }
           })
           .catch(error => console.error("There was a problem with the fetch operation:", error));
-    }
+    },
+    validateDateLength(event) {
+      const date = event.target.value;
+      const year = date.split('-')[0];
+      if (year && year.length > 4) {
+        event.target.value = '';
+        alert("O ano não pode ter mais de 4 dígitos.");
+      }
+    },
   },
   mounted() {
     this.fetchTasks();
   }
 };
+
 </script>
 
 <style scoped>
